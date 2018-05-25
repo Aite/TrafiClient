0124aabcf06b3b40360883f605fd25d19de53b19
Alaa Al-Zaibak <alaaite.passion@gmail.com>
2018-05-25 18:28:14 +0300

    Initial Commit

---
+++ VoltLinesExercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoltLinesExercise
+//
+//  Created by Alaa Al-Zaibak on 25.05.2018.
+//  Copyright © 2018 Alaa Al-Zaibak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
